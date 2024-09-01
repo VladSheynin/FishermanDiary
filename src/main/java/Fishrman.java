b567a439671d0b5cfa5c50ead0253142179503b4
V.Sheynin <sheynin_vlad@rambler.ru>
2024-09-01 15:02:00 +0300

    Create 2 classes

---
+++ src/main/java/Fishrman.java
@@ -0,0 +1,3 @@
+public class Fishrman extends Person{
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private String family;
+    private String name;
+    private String Surname;
+    private int age;
+
+    public Person(String family, String name, String surname, int age) {
+        this.family = family;
+        this.name = name;
+        Surname = surname;
+        this.age = age;
+    }
+
+}
